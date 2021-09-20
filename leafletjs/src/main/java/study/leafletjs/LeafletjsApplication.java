f3f0a43ee39c328cd4cdd872e1ad291a6f15e6c9
aamoos <aamoos@naver.com>
2021-09-20 21:36:31 +0900

    초기설정

---
+++ leafletjs/src/main/java/study/leafletjs/LeafletjsApplication.java
@@ -0,0 +1,13 @@
+package study.leafletjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafletjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafletjsApplication.class, args);
+	}
+
+}
